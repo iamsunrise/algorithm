19d2479e50341e5e7a083b684d38d0aabd848a2b
131 <131@163.com>
2019-09-06 14:32:24 +0800

    create singlinklist

---
+++ singLinkList.py
@@ -0,0 +1,42 @@
+class Node(object):
+    def __init__(self,item):
+        self.next = None
+        self.item = item
+
+class SingLinkList(object):
+    def isEmpty(self):
+        """是否为空"""
+        pass
+
+    def length(self):
+        """是否为空"""
+        pass
+
+    def travel(self):
+        """是否为空"""
+        pass
+
+    def append(self):
+        """是否为空"""
+        pass
+
+    def add(self):
+        """是否为空"""
+        pass
+
+    def insert(self,pos,item):
+        """是否为空"""
+        pass
+
+    def del(self,pos)
+        """是否为空"""
+        pass
+
+    def search(self,item):
+        """检查节点是否存在"""
+        pass
+        
+    def remove(self,item):
+        """删除节点"""
+        pass
+
